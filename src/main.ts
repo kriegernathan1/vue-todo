8dd006e910081ac470b8662b7a9df4697d449137
Nathan Krieger <nkrieger0444@gmail.com>
2023-11-04 09:41:05 -0700

    Add Vuetify with fa-5 icon set

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
-import './assets/main.css'
+import "@fortawesome/fontawesome-free/css/all.css"; // Ensure your project is capable of handling css files
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-app.use(createPinia())
-app.use(router)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
