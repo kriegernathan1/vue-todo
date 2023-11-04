6bae5a77a8175cf5d8e875c7fa9f316e0b9fbfe5
Nathan Krieger <nkrieger0444@gmail.com>
2023-11-04 11:56:02 -0700

    update login user store method

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -7,12 +7,35 @@ interface User {
   lastName: string;
 }
 
+interface LoginResponse {
+  status: "Success" | "Failure";
+  error?: any;
+  user: User;
+}
+
 export const useCounterStore = defineStore("user", () => {
   const user = ref<User>();
 
-  async function login(username: string, password: string) {}
+  async function login(
+    username: string,
+    password: string
+  ): Promise<LoginResponse> {
+    const userResponse = await fetch(
+      "/mock-data/person-175c421a-6e9e-49cc-97cd-d0b679dbb4d5.json"
+    );
+    const user = await userResponse.json();
+    user.value = user;
 
-  function logout() {}
+    return {
+      status: "Success",
+      user,
+    };
+  }
+
+  function logout(): boolean {
+    user.value = undefined;
+    return true;
+  }
 
   async function isAuthenticated() {
     return !!user.value;
