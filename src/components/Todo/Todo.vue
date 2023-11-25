f218e2992aa3fbeb818f9d4ecee2c8f1182a138d
Nathan Krieger <nkrieger0444@gmail.com>
2023-11-25 10:52:34 -0800

    fetch todos from fake API

--- src/components/Todo/Todo.vue
+++ src/components/Todo/Todo.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TodoItem from "./TodoItem.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { Urls } from '../../constants/urls';
 
 export interface TodoItem {
   name: string;
@@ -8,30 +9,66 @@ export interface TodoItem {
   id: string;
 }
 
-const items = ref<TodoItem[]>([
-  {
-    name: "Clean Kitched",
-    isComplete: false,
-    id: "1",
-  },
-  {
-    name: "Vacuum Living Room",
-    isComplete: false,
-    id: "2",
-  },
-  {
-    name: "Pay Water Bill",
-    isComplete: false,
-    id: "3",
-  },
-  {
-    name: "Walk the dog",
-    isComplete: false,
-    id: "4",
-  },
-]);
+interface TodoResponseItem {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
 
+type TodoResponse = TodoResponseItem[]
+
+const items = ref<TodoItem[]>([]);
 const newTodoText = ref("");
+const isLoading = ref(false);
+const MAX_ITEMS_SHOWN = 20;
+
+onMounted(async () => {
+  try {
+    isLoading.value = true;
+    const todosResponse = await fetch(Urls.USER_TODOS);
+    const todoItems = await todosResponse.json() as TodoResponse;
+
+    handleTodoItems(todoItems);
+  }
+  catch (e) {
+    console.log("error fetching todos", e)
+  }
+  finally {
+    isLoading.value = false;
+  }
+})
+
+function handleTodoItems(todoItems: TodoResponse) {
+  let tempItems: TodoItem[] = [];
+  todoItems.forEach((todo: TodoResponseItem) => {
+    const newTodo: TodoItem = {
+      name: todo.title,
+      isComplete: todo.completed,
+      id: (todo.id || '').toString(),
+    }
+    tempItems.push(newTodo);
+  })
+  tempItems = filterAndSortItems(tempItems);
+
+  items.value = tempItems;
+}
+
+function filterAndSortItems(todoItems: TodoItem[]): TodoItem[] {
+  const completedItems: TodoItem[] = [];
+  const outstandingItems: TodoItem[] = [];
+
+  todoItems.forEach((todo) => {
+    let bucket = todo.isComplete ? completedItems : outstandingItems
+    bucket.push(todo)
+  })
+
+  const sortedTodos = [...outstandingItems.slice(0, MAX_ITEMS_SHOWN / 2), ...completedItems.slice(0, MAX_ITEMS_SHOWN / 2)]
+
+  if (sortedTodos.length < MAX_ITEMS_SHOWN) return sortedTodos
+
+  return sortedTodos.slice(0, MAX_ITEMS_SHOWN);
+}
 
 function createTodo() {
   items.value.unshift({
@@ -63,7 +100,11 @@ function markComplete(id: string, isComplete: boolean) {
       <v-text-field v-model="newTodoText" placeholder="Enter new Task" @keyup.enter="createTodo"></v-text-field>
     </div>
 
-    <div class="todo-items-container">
+    <div class="progress-indicator" v-if="isLoading">
+      <v-progress-circular indeterminate></v-progress-circular>
+    </div>
+
+    <div class="todo-items-container" v-if="!isLoading">
       <div class="todo-list-container" v-for="task of items">
         <div class="todo-item">
           <TodoItem :task="task" @removeTask="removeTask" @markComplete="markComplete" />
@@ -78,6 +119,10 @@ function markComplete(id: string, isComplete: boolean) {
   display: flex;
   flex-direction: column;
   width: 500px;
+}
+
+.progress-indicator {
+  align-self: center;
 }
 
 .todo-items-container {
---
+++ src/constants/urls.ts
@@ -0,0 +1,7 @@
+interface URL {
+  url: string;
+}
+
+export enum Urls {
+  USER_TODOS = "https://jsonplaceholder.typicode.com/todos",
+}
